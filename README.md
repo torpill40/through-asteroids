81d6c0adf4f4ad9bd1fa7e76b019fd8bb8c94fa9
Louis-Maël Derval <louismael40@gmail.com>
2020-05-22 16:04:10 +0200

    Création du projet Through Asteroids

---
+++ README.md
@@ -0,0 +1,3 @@
+# Through Asteroids :
+
+Un jeu d'arcade où l'on conduit un vaisseau à travers des astéroïdes.
---
+++ ThroughAsteroids/src/main/java/com/torpill/throughasteroids/App.java
@@ -0,0 +1,14 @@
+package com.torpill.throughasteroids;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+	public static final Logger LOGGER = LogManager.getLogger("Through Asteroids");
+
+	public static void main(String[] args) {
+
+		LOGGER.debug("App Started !");
+	}
+}
